41d91b5fa761aff440ef07e6d143932519f740bb
Elves Sousa <elves.sousa@gmail.com>
2021-04-19 14:37:23 -0300

    React Native - Day 1

---
+++ plant-manager/src/components/Button.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from 'react-native';
+import colors from '../styles/colors';
+
+interface ButtonProps extends TouchableOpacityProps {
+  title: string;
+}
+
+export function Button({ title, ...props }: ButtonProps) {
+  return (
+    <TouchableOpacity style={styles.button} activeOpacity={0.8} {...props}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginRight: 10,
+    padding: 10,
+    minWidth: 56,
+    minHeight: 56,
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 24,
+  },
+});
