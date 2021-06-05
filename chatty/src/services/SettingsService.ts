9674fa12f40c0a8c9b594731d6981032d6a29cfe
Elves Sousa <elves.sousa@gmail.com>
2021-06-05 17:38:45 -0300

    Node - Day 3

--- chatty/src/services/SettingsService.ts
+++ chatty/src/services/SettingsService.ts
@@ -1,5 +1,6 @@
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, Repository } from 'typeorm';
 import { SettingsRepository } from '../repositories/SettingsRepository';
+import { Setting } from '../entities/Setting';
 
 interface ISettingsCreate {
   chat: boolean;
@@ -7,16 +8,23 @@ interface ISettingsCreate {
 }
 
 export class SettingsService {
+  private settingsRepository: Repository<Setting>;
+
+  constructor() {
+    this.settingsRepository = getCustomRepository(SettingsRepository);
+  }
+
   async create({ chat, username }: ISettingsCreate) {
-    const settingsRepository = getCustomRepository(SettingsRepository);
-    const settings = settingsRepository.create({ chat, username });
-    const userAlreadyExists = await settingsRepository.findOne({ username });
+    const settings = this.settingsRepository.create({ chat, username });
+    const userAlreadyExists = await this.settingsRepository.findOne({
+      username,
+    });
 
     if (userAlreadyExists) {
       throw new Error('User already exists!');
     }
 
-    await settingsRepository.save(settings);
+    await this.settingsRepository.save(settings);
 
     return settings;
   }
